40837ebd8303ba2dc172dfaf20f54de32d296a7e
CptMoore <39010654+CptMoore@users.noreply.github.com>
2018-05-11 01:45:26 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,36 @@
+# SpeedMod
+BattleTech mod (using ModTek and DynModLib) that allows to speed up gameplay.
+
+## Requirements
+** Warning: Uses the experimental BTML mod loader and ModTek **
+
+either
+* install BattleTechModTools using [BattleTechModInstaller](https://github.com/CptMoore/BattleTechModTools/releases)
+
+or
+* install [BattleTechModLoader](https://github.com/Mpstark/BattleTechModLoader/releases) using [instructions here](https://github.com/Mpstark/BattleTechModLoader)
+* install [ModTek](https://github.com/Mpstark/ModTek/releases) using [instructions here](https://github.com/Mpstark/ModTek)
+* install [DynModLib](https://github.com/CptMoore/DynModLib/releases) using [instructions here](https://github.com/CptMoore/DynModLib)
+
+## Features
+
+Press P to activate faster gameplay, press P again to slow it to normal speeds.
+
+BattleTech already has a feature to enable faster gameplay, but that was only available through the debug menu. With this mod you now can easily make the game work faster or even slower.
+
+Setting | Type | Default | Description
+--- | --- | --- | ---
+speedFactor | float | 5 | 1 = normal speed, set this higher or lower to make the speed mod faster or slower.
+
+## Download
+
+Downloads can be found on [github](https://github.com/CptMoore/SpeedMod/releases).
+
+## Install
+
+After installing BTML, ModTek and DynModLib, put the mod into the \BATTLETECH\Mods\ folder and launch the game.
+
+## Development
+
+* Use git
+* Use Visual Studio or DynModLib to compile the project
---
+++ source/Control.cs
@@ -0,0 +1,112 @@
+﻿
+using System;
+using HBS.Logging;
+using Harmony;
+using System.Reflection;
+using BattleTech;
+using BattleTech.UI;
+using DynModLib;
+using InControl;
+using UnityEngine;
+using Logger = HBS.Logging.Logger;
+
+namespace SpeedMod
+{
+    public static class Control
+    {
+        public static Mod mod;
+
+        public static SpeedSettings settings = new SpeedSettings();
+
+        public static void OnInit(string modDirectory)
+        {
+            mod = new Mod(modDirectory);
+            mod.LoadSettings(settings);
+			
+			var harmony = HarmonyInstance.Create(mod.Name);
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+            // logging output can be found under BATTLETECH\BattleTech_Data\output_log.txt
+            // or also under yourmod/log.txt
+            mod.Logger.Log("Loaded " + mod.Name);
+        }
+
+        public static PlayerAction SpeedUpAction;
+        public static bool speedToggled;
+
+
+        public class SpeedSettings : ModSettings
+        {
+            public float speedFactor = 5.0f;
+        }
+
+        [HarmonyPatch(typeof(DynamicActions), "CreateWithDefaultBindings")]
+        public static class DynamicActionsCreateWithDefaultBindingsPatch
+        {
+            public static void Postfix(DynamicActions __result)
+            {
+                try
+                {
+                    var adapter = new DynamicActionsAdapter(__result);
+                    SpeedUpAction = adapter.CreatePlayerAction("Toggle Speed Mod");
+                    SpeedUpAction.AddDefaultBinding(Key.P);
+                    mod.Logger.Log("added dynamic action and default key binding");
+                }
+                catch (Exception e)
+                {
+                    mod.Logger.LogError(e);
+                }
+            }
+        }
+
+        internal class DynamicActionsAdapter : Adapter<DynamicActions>
+        {
+            internal DynamicActionsAdapter(DynamicActions instance) : base(instance)
+            {
+            }
+
+            internal PlayerAction CreatePlayerAction(string name)
+            {
+                return traverse.Method("CreatePlayerAction", name).GetValue<PlayerAction>(name); ;
+            }
+        }
+
+        [HarmonyPatch(typeof(CombatSelectionHandler), "ProcessInput")]
+        public static class CombatSelectionHandlerProcessInputPatch
+        {
+            public static void Prefix(CombatSelectionHandler __instance)
+            {
+                try
+                {
+                    if (SpeedUpAction == null || !SpeedUpAction.HasChanged || !SpeedUpAction.IsPressed)
+                    {
+                        return;
+                    }
+
+                    speedToggled = !speedToggled;
+                    
+                    mod.Logger.Log("toggled speed to factor " + settings.speedFactor);
+                }
+                catch (Exception e)
+                {
+                    Control.mod.Logger.LogError(e);
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(StackManager), "get_deltaTime")]
+        public static class StackManagerget_deltaTimePatch
+        {
+            public static bool Prefix(StackManager __instance, ref float __result)
+            {
+                if (speedToggled)
+                {
+                    __result = Time.deltaTime * settings.speedFactor;
+                    return false;
+                }
+
+                return true;
+            }
+        }
+    }
+}
